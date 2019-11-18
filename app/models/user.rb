3ee4b8a1aeac73041ba2e13211d1c33782abdc62
yorickov <yorickov@gmail.com>
2019-11-19 02:27:39 +0300

    add User method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  def tests_by_level(level); end
+  def tests_by_level(level)
+    Test
+      .joins('INNER JOIN test_results ON tests.id = test_results.test_id')
+      .where('test_results.user_id = ? AND tests.level = ?', self.id, level)
+      .select(:title)
+  end
 end
