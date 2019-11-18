cfa672d6183fce7c9f2536ac32215e0ba29190e0
yorickov <yorickov@gmail.com>
2019-11-19 01:21:11 +0300

    add Test method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  class << self
+    def tests_by_category(category)
+      Test
+        .joins('INNER JOIN categories ON tests.category_id = categories.id')
+        .where('categories.title = ?', category)
+        .pluck(:title)
+        .sort { |a, b| b <=> a }
+    end
+  end
 end
