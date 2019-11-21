c2d1b386d447d5d3119dc9f4a8eb9883b10cf513
yorickov <yorickov@gmail.com>
2019-11-21 19:53:25 +0300

    fix scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -10,21 +10,16 @@ class Test < ApplicationRecord
     only_integer: true, greater_than_or_equal_to: 0
   }
 
-  scope :by_category, ->(category) {
-    Test
-      .unscoped # Rails 6.1: no inherit scoping
-      .joins(:category)
-      .where(categories: { title: category })
-      .order(title: :desc)
+  scope :by_category, ->(category_title) {
+    joins(:category).where(categories: { title: category_title })
   }
-
   scope :simple, -> { where(level: [0, 1]) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
 
   class << self
     def tests_by_category(category)
-      by_category(category).pluck(:title)
+      by_category(category).order(title: :desc).pluck(:title)
     end
   end
 end
