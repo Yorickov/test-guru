564e7c1e897f09577c91fd183a9c526b76447987
yorickov <yorickov@gmail.com>
2019-12-01 20:02:45 +0300

    add basic test-passage logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,19 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :own_tests, class_name: 'Test', inverse_of: 'author'
-  has_many :test_results
-  has_many :tests, through: :test_results
+  has_many :test_passages
+  has_many :tests, through: :test_passages
 
   validates :first_name, :last_name, :email, presence: true
 
   def tests_by_level(level)
     Test
-      .joins(:test_results)
-      .where(test_results: { user_id: id })
+      .joins(:test_passages)
+      .where(test_passages: { user_id: id })
       .where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
---
+++ test/controllers/test_passages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class TestPassagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get test_passages_show_url
+    assert_response :success
+  end
+
+  test "should get result" do
+    get test_passages_result_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get test_passages_update_url
+    assert_response :success
+  end
+
+end
