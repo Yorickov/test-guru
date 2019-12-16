60b69b7e4c47423db588bdffd1fa86f93369c455
yorickov <yorickov@gmail.com>
2019-12-17 01:40:48 +0300

    bootstrap resourses: admin-answers, admin-questions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,6 @@
 module QuestionsHelper
   def question_header(question)
-    prefix = question.persisted? ? t('.header_edit') : t('.header_create')
-    "#{prefix} #{question.test.title}"
+    question.persisted? ? t('.header_edit') : t('.header_create')
+    # "#{prefix} #{question.test.title}"
   end
 end
