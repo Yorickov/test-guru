9e916c43b5976504fa6e6463a242dda9023e9e0d
yorickov <yorickov@gmail.com>
2019-12-22 18:29:04 +0300

    add inline-form and DOM-handler -without jQuery

---
+++ app/javascript/utilities/form_inline.js
@@ -0,0 +1,40 @@
+const formInlineHandler = (testId) => {
+  const link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]');
+  const testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]');
+  const formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]');
+
+  if (formInline.classList.contains('hide')) {
+    testTitle.classList.add('hide');
+    formInline.classList.remove('hide');
+    link.textContent = 'Cancel';
+  } else {
+    testTitle.classList.remove('hide');
+    formInline.classList.add('hide');
+    link.textContent = 'Edit';
+  }
+};
+
+const formInlineLinkHandler = (e) => {
+  e.preventDefault();
+  const target = e.currentTarget;
+
+  // getAttribute if data-* not supported
+  const testId = target.dataset.testId;
+  formInlineHandler(testId);
+};
+
+export default () => {
+  const controls = document.querySelectorAll('.form-inline-link');
+  if (controls.length) {
+    controls.forEach(control => control.addEventListener(
+      'click',
+      formInlineLinkHandler.bind(this),
+    ));
+  }
+
+  const errors = document.querySelector('.resource-errors');
+  if (errors) {
+    const resourceId = errors.dataset.resourceId;
+    formInlineHandler(resourceId);
+  }
+};
--- app/javascript/utilities/index.js
+++ app/javascript/utilities/index.js
@@ -1,9 +1,12 @@
 import sorting from './sorting';
 import validation from './validation';
+import formInline from './form_inline';
+
 
 export default () => {
   document.addEventListener('turbolinks:load', () => {
     sorting();
     validation();
+    formInline();
   });
 }
