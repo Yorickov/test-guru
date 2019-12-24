a979e463a440d66f49d5e4b4a82b72278592da65
yorickov <yorickov@gmail.com>
2019-12-25 02:59:20 +0300

    fight with heroku

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,7 +3,10 @@ class TestPassage < ApplicationRecord
 
   belongs_to :user
   belongs_to :test
-  belongs_to :current_question, class_name: 'Question', optional: true
+  belongs_to :current_question,
+             class_name: 'Question',
+             foreign_key: 'question_id',
+             optional: true
 
   before_validation :before_validation_set_first_question, on: :create
   before_update :before_update_set_next_question
