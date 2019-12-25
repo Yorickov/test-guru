2afca6eed73694d03f5bb4cd87b35bb24669993d
yorickov <yorickov@gmail.com>
2019-12-25 17:22:53 +0300

    hrk: fix css-compile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import 'bootstrap/dist/js/bootstrap';
+// import 'bootstrap/dist/js/bootstrap';
+import '../../../node_modules/bootstrap/dist/js/bootstrap';
 import '../stylesheets/application.scss';
 
 import utilities from '../utilities';
