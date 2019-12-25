b649d2270053f78a4f892b27d265a51781e3892f
yorickov <yorickov@gmail.com>
2019-12-25 03:43:57 +0300

    fight with heroku

--- db/migrate/20191225085704_create_test_passages.rb
+++ db/migrate/20191225085704_create_test_passages.rb
@@ -3,9 +3,9 @@ class CreateTestPassages < ActiveRecord::Migration[6.0]
     create_table :test_passages do |t|
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
-      t.references :current_question, foreign_key: true
+      t.integer :current_question_id
       t.integer :correct_questions, default: 0
-      t.index [:test_id, :user_id], unique: true
+      t.index :current_question_id
 
       t.timestamps
     end
